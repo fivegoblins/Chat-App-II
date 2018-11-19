fc78d1dcafd5fafaaaa347a5302fa1f0e8ab7f32
Alexandra <alexswartz9753@gmail.com>
2018-11-19 10:31:37 -0500

    wrote server routes for get and post

--- server.js
+++ server.js
@@ -5,10 +5,24 @@ const port = 8000;
 const db = require('./data/dbConfig.js');
 
 server.use(express.static(__dirname));
+server.use(expres.json());
 
+server.get('/messages', (req, res)=> {
+    Message.find({}, (err, messages)=> {
+        res.send(messages);
+    });
+});
 
-
-
+server.post('/messages', (req, res)=> {
+    const message = new Message(req.body);
+    message.save((err)=> {
+        if(err) {
+            res.send(500);
+        } else {
+            res.send(200);
+        }
+    })
+});
 
 
 server.listen(port, ()=> {
