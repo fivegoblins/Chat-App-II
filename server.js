d472fec254ac9c0ff6b9cbbaecb3df8f3e2357fd
Alexandra <alexswartz9753@gmail.com>
2018-11-19 11:30:04 -0500

    organizational changes to server.js and index.html

--- server.js
+++ server.js
@@ -1,48 +1,47 @@
 const express = require('express');
-const server = express();
+const app = express();
+const http = require('http').Server(app);
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const db = 'mongodb://alexswartz:sunnyday4!@ds247688.mlab.com:47688/chat-messages';
-const http = require('http').Server(server);
 const io = require('socket.io')(http);
-const port = 8000;
 
-mongoose.connect(db, (err)=> {
-    console.log('mongodb connected', err);
-});
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const Message = mongoose.model('Message', {
     name: String,
     message: String
 });
 
-server.use(express.static(__dirname));
-server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({ extended: false }));
+const dbUrl = 'mongodb://alexswartz:sunnyday4!@ds247688.mlab.com:47688/chat-messages';
 
-io.on('connection', ()=> {
-    console.log('user connected');
-});
-
-server.get('/messages', (req, res)=> {
+app.get('/messages', (req, res)=> {
     Message.find({}, (err, messages)=> {
         res.send(messages);
     });
 });
 
-server.post('/messages', (req, res)=> {
+app.post('/messages', (req, res)=> {
     const message = new Message(req.body);
     message.save((err)=> {
         if(err) {
-            res.send(500);
+            res.sendStatus(500);
         } else {
             io.emit('message', req.body);
-            res.send(200);
+            res.sendStatus(200);
         }
     });
 });
 
-
-server.listen(port, ()=> {
-    console.log(`server is listening on port ${port}`);
+io.on('connection', ()=> {
+    console.log('user connected');
 });
+
+mongoose.connect(dbUrl, (err)=> {
+    console.log('mongodb connected', err);
+});
+
+const server = http.listen(8000, ()=> {
+    console.log('server is running on port 8000', server.address().port);
+})
